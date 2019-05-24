57cb4412328396ae609db4d898f0ec68c44c2e18
yucea@esjee.co.kr <yucea@esjee.co.kr>
2019-05-24 11:15:37 +0900

    first commit

---
+++ ranker/src/main/java/kr/co/esjee/ranker/RankerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.esjee.ranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankerApplication.class, args);
+	}
+
+}
---
+++ ranker/src/test/java/kr/co/esjee/ranker/RankerApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.esjee.ranker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RankerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
