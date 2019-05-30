49c32d837f0325d3a6660c8e413239b2657a0ef8
Stony <maxbrain@naver.com>
2019-05-30 14:56:10 +0900

    web crawler test

--- ranker/src/test/java/kr/co/esjee/ranker/crawler/TestCrawler.java
+++ ranker/src/test/java/kr/co/esjee/ranker/crawler/TestCrawler.java
@@ -4,17 +4,28 @@ import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import kr.co.esjee.ranker.crawler.Crawler.CrawlerVO;
+import kr.co.esjee.ranker.webapp.model.Article;
+import kr.co.esjee.ranker.webapp.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @Slf4j
 public class TestCrawler {
+	
+	@Autowired
+	private ArticleService articleService;
 
 	@Test
-	public void testCrawler() {
+	public void testCrawler(){
 		
 		String url = "https://media.daum.net/ranking/popular";
+		String[] urlParams = {"regDate=20190530"};
 		String listAtrb = "ul.list_news2";
 		String listEachAtrb = "li";
 		String titleAtrb = "h3.tit_view";
@@ -23,12 +34,13 @@ public class TestCrawler {
 		try {
 			Crawler crawler = new Crawler();
 			
-			List<CrawlerVO> resultList = crawler.execute(url, listAtrb, listEachAtrb, titleAtrb, contentAtrb);
+			List<Article> articleList = crawler.execute(url, urlParams, listAtrb, listEachAtrb, titleAtrb, contentAtrb);
 			
-			for (CrawlerVO crawlerVO : resultList) {
-				log.info("{}", crawlerVO.toString());
+			for (Article article : articleList) {
+				article.setWriter("Test");
+				article.setCreated("2019-05-30");
+				articleService.save(article);
 			}
-			
 		} catch (IOException e) {
 			log.error("Error = {}", e.getLocalizedMessage());
 		}
