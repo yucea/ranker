8e206fd1a85e35f8206b1e2182a258e88c209d5f
Stony <maxbrain@naver.com>
2019-05-30 10:37:42 +0900

    Test Crawler

--- ranker/src/test/java/kr/co/esjee/ranker/crawler/TestCrawler.java
+++ ranker/src/test/java/kr/co/esjee/ranker/crawler/TestCrawler.java
@@ -1,11 +1,36 @@
 package kr.co.esjee.ranker.crawler;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
 
+import kr.co.esjee.ranker.crawler.Crawler.CrawlerVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TestCrawler {
 
 	@Test
 	public void testCrawler() {
-
+		
+		String url = "https://media.daum.net/ranking/popular";
+		String listAtrb = "ul.list_news2";
+		String listEachAtrb = "li";
+		String titleAtrb = "h3.tit_view";
+		String contentAtrb = "div.news_view";
+		
+		try {
+			Crawler crawler = new Crawler();
+			
+			List<CrawlerVO> resultList = crawler.execute(url, listAtrb, listEachAtrb, titleAtrb, contentAtrb);
+			
+			for (CrawlerVO crawlerVO : resultList) {
+				log.info("{}", crawlerVO.toString());
+			}
+			
+		} catch (IOException e) {
+			log.error("Error = {}", e.getLocalizedMessage());
+		}
 	}
 }
