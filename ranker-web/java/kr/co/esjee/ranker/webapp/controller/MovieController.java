280f2b650e025123b3fabeda33a97bf636acf1ce
Stony <maxbrain@naver.com>
2019-06-21 16:06:52 +0900

    init

--- ranker-web/java/kr/co/esjee/ranker/webapp/controller/MovieController.java
+++ ranker-web/java/kr/co/esjee/ranker/webapp/controller/MovieController.java
@@ -8,22 +8,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.esjee.ranker.webapp.model.Person;
-import kr.co.esjee.ranker.webapp.service.PersonService;
+import kr.co.esjee.ranker.webapp.model.Movie;
+import kr.co.esjee.ranker.webapp.service.MovieService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/rest/person")
+@RequestMapping("/rest/movie")
 @Slf4j
-public class PersonController extends AppController {
+public class MovieController extends AppController {
 	
 	@Autowired
-	private PersonService personService;
+	private MovieService movieService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String list(
@@ -37,18 +39,35 @@ public class PersonController extends AppController {
 		
 		try {
 			
-			Page<Person> page = personService.findAll(super.getPageable(start, length, Sort.by(Direction.DESC, PID)));
+			Page<Movie> page = movieService.findAll(super.getPageable(start, length, Sort.by(Direction.DESC, TID)));			
 
 			result.put(DATA, page.getContent());
 			result.put(DRAW, draw);
 			result.put(RECORDS_TOTAL, page.getTotalElements());
 			result.put(RECORDS_FILTERED, page.getTotalElements());
 		} catch (Exception e) {
-			e.printStackTrace();
 			log.error(e.getLocalizedMessage());
 			result.put(SUCCESS, false);
 			result.put(MESSAGE, e.getLocalizedMessage());
 		}		
+
+		return result.toString();
+	}
+	
+	@RequestMapping(value = "/info/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String info(@PathVariable long id, Model model) {
+		JSONObject result = new JSONObject();
+
+		try {
+			Movie movie = movieService.findById(id);
+
+			result.put(SUCCESS, true);
+			result.put(DATA, new JSONObject(movie));
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put(SUCCESS, false);
+			result.put(MESSAGE, e.getLocalizedMessage());
+		}
 
 		return result.toString();
 	}
--- ranker-web/java/kr/co/esjee/ranker/webapp/controller/PersonController.java
+++ ranker-web/java/kr/co/esjee/ranker/webapp/controller/PersonController.java
@@ -8,22 +8,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.esjee.ranker.webapp.model.Movie;
-import kr.co.esjee.ranker.webapp.service.MovieService;
+import kr.co.esjee.ranker.webapp.model.Person;
+import kr.co.esjee.ranker.webapp.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/rest/movie")
+@RequestMapping("/rest/person")
 @Slf4j
-public class MovieController extends AppController {
+public class PersonController extends AppController {
 	
 	@Autowired
-	private MovieService movieService;
+	private PersonService personService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String list(
@@ -37,17 +39,36 @@ public class MovieController extends AppController {
 		
 		try {
 			
-			Page<Movie> page = movieService.findAll(super.getPageable(start, length, Sort.by(Direction.DESC, TID)));			
+			Page<Person> page = personService.findAll(super.getPageable(start, length, Sort.by(Direction.DESC, PID)));
 
 			result.put(DATA, page.getContent());
 			result.put(DRAW, draw);
 			result.put(RECORDS_TOTAL, page.getTotalElements());
 			result.put(RECORDS_FILTERED, page.getTotalElements());
 		} catch (Exception e) {
+			e.printStackTrace();
 			log.error(e.getLocalizedMessage());
 			result.put(SUCCESS, false);
 			result.put(MESSAGE, e.getLocalizedMessage());
 		}		
+
+		return result.toString();
+	}
+	
+	@RequestMapping(value = "/info/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String info(@PathVariable long id, Model model) {
+		JSONObject result = new JSONObject();
+
+		try {
+			Person person = personService.findById(id);
+
+			result.put(SUCCESS, true);
+			result.put(DATA, new JSONObject(person));
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put(SUCCESS, false);
+			result.put(MESSAGE, e.getLocalizedMessage());
+		}
 
 		return result.toString();
 	}
