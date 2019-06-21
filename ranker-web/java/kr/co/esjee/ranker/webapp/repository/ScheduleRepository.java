32c3755332b9fb91d4aaa1e239ff4ae1b57cb8b2
Stony <maxbrain@naver.com>
2019-06-21 17:47:14 +0900

    schedule list update

---
+++ ranker-web/java/kr/co/esjee/ranker/webapp/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package kr.co.esjee.ranker.webapp.repository;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import kr.co.esjee.ranker.webapp.model.Schedule;
+
+public interface ScheduleRepository extends ElasticsearchRepository<Schedule, Long> {
+	
+}
