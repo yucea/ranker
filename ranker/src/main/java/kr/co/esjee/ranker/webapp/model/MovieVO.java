58167c457be7b5699816515be539cad97cc10afe
Stony <maxbrain@naver.com>
2019-06-21 16:33:01 +0900

    basicUrl Empty

--- ranker/src/main/java/kr/co/esjee/ranker/webapp/model/MovieVO.java
+++ ranker/src/main/java/kr/co/esjee/ranker/webapp/model/MovieVO.java
@@ -28,7 +28,7 @@ public class MovieVO implements AppConstant {
 	private String listUrl = "";
 	
 	// Movie URL
-	private String basicUrl = "https://movie.naver.com/movie/bi/pi/basic.nhn?code=183820";
+	private String basicUrl = "";
 	
 	// Person URL
 	private String crewUrl = "https://movie.naver.com/movie/bi/mi/detail.nhn";	
