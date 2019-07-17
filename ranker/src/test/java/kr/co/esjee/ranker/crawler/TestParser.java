ff562940dd7072abab6718711ee02d62f807fc52
Stony <maxbrain@naver.com>
2019-07-17 12:41:32 +0900

    Refactoring

---
+++ ranker/src/test/java/kr/co/esjee/ranker/crawler/TestParser.java
@@ -0,0 +1,13 @@
+package kr.co.esjee.ranker.crawler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class TestParser {
+
+    @Test
+    public void testParser () {
+        System.out.println("111");
+    }
+}
